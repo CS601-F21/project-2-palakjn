194b828a5eff5c34c2aafe5318aeacf9dee6f0a7
palakjn <palak29jain05@gmail.com>
2021-09-28 18:42:58 -0700

    Bug fix on time taking

--- src/main/java/cs601/project2/controllers/testApplication/ReviewListener.java
+++ src/main/java/cs601/project2/controllers/testApplication/ReviewListener.java
@@ -4,15 +4,32 @@ import cs601.project2.configuration.Constants;
 import cs601.project2.controllers.framework.implementation.Subscriber;
 import cs601.project2.models.Review;
 import cs601.project2.view.JsonManager;
-import cs601.project2.view.OutputManager;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ReviewListener extends Subscriber<Review> {
     private String fileLocation;
     private Constants.REVIEW_OPTION reviewOption;
+    private List<Review> reviewList;
+    private int maxValue = 1000;
+    private BufferedWriter bufferedWriter;
 
     public ReviewListener(String fileLocation, Constants.REVIEW_OPTION reviewOption) {
         this.fileLocation = fileLocation;
         this.reviewOption = reviewOption;
+        this.reviewList = new ArrayList<>();
+        try {
+            this.bufferedWriter = Files.newBufferedWriter(Paths.get(fileLocation), StandardCharsets.ISO_8859_1);
+        }
+        catch (IOException ioException) {
+            //TODO
+        }
     }
 
     @Override
@@ -27,8 +44,23 @@ public class ReviewListener extends Subscriber<Review> {
             String json = JsonManager.toJson(review);
 
             if(json != null) {
-                OutputManager.writeToFile(fileLocation, json);
+                try{
+                    bufferedWriter.write(json);
+                    bufferedWriter.newLine();
+                }
+                catch (IOException ioException) {
+                    System.out.printf("Unable to write to a file %s. %s\n", fileLocation, ioException.getMessage());
+                }
             }
+        }
+    }
+
+    public void flush() {
+        try {
+            bufferedWriter.close();
+        }
+        catch (IOException ioException) {
+
         }
     }
 }
